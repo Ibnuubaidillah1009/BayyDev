5376eca1406b9c501a37e0ebab53e1a39c737637
Ibnuubaidillah1009 <ibnuubaidillah1009@gmail.com>
2026-07-15 13:41:26 +0700

    update certificate

--- src/lib/certificates-data.ts
+++ src/lib/certificates-data.ts
@@ -1,9 +1,9 @@
 export const certificatesData = [
   {
-    title: "LKS Bidang Web Technologies Tingkat Kabupaten",
-    issuer: "Dinas Pendidikan Kabupaten Pasuruan",
+    title: "2nd Place – LKS Web Technologies (Regency Level)",
+    issuer: "Cabang Dinas Pendidikan Wilayah Pasuruan",
     date: "2026",
-    description: "I Won Second Place at the Pasuruan Regency Level.",
+    description: "Won Second Place in the LKS Web Technologies Competition at the Pasuruan Regency Level.",
     image: "/images/cert-1.jpeg",
   },
   {
@@ -19,5 +19,12 @@ export const certificatesData = [
     date: "2026",
     description: "Comprehensive training on building full-stack web applications.",
     image: "/images/cert-3.png",
+  },
+    {
+    title: "Medallion for Excellence – LKS Web Technologies (Provincial Level)",
+    issuer: "Dinas Pendidikan Provinsi Jawa Timur",
+    date: "2026",
+    description: "Received the Medallion for Excellence in the 2026 East Java Provincial LKS Web Technologies Competition.",
+    image: "/images/cert-4.jpeg",
   },
 ];
